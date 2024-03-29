1c3a0da118fed183242c9457ef47eec85a39017a
n1kys <lebednikita12@gmail.com>
2024-03-29 20:22:23 +0200

    added properties and a constructor to the Book class

--- LibraryApp/ClasessForWorkWithData/Book.cs
+++ LibraryApp/ClasessForWorkWithData/Book.cs
@@ -11,6 +11,28 @@ namespace ClasessForWorkWithData
     // Кожна книга повинна містити інформацію про її назву, автора, жанр, рік написання, статус аренди.
     public class Book
     {
-        
+        // Назва книги
+        public string Title { get; set; }
+        // Автор
+        public string Author { get; set; }
+        // Жанр
+        public string Genre { get; set; }
+        // Рік написання
+        public int Year { get; set; }
+        // статус оренди книги 
+        public string Rent_status { get; set; }
+        // історія оренди книги читачами
+        public List<string> Rent_history { get; set; }
+
+        // конструктор для заповнення назви книги, автору, жанру, року написання, статусу оренди та історії оренди
+        public Book(string title, string author, string genre, int year, string rent_status, List<string> rent_history)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+            Year = year;
+            Rent_status = rent_status;
+            Rent_history = rent_history;
+        }
     }
 }
