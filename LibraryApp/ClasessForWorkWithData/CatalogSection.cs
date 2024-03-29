9d27a8fa3f26ebb72986a7efd19a6203f14e354e
n1kys <lebednikita12@gmail.com>
2024-03-29 20:45:49 +0200

    added properties and a constructor for the CatalogSection class

--- LibraryApp/ClasessForWorkWithData/CatalogSection.cs
+++ LibraryApp/ClasessForWorkWithData/CatalogSection.cs
@@ -9,9 +9,19 @@ namespace ClasessForWorkWithData
     // Клас CatalogSection аналогічно з класом Book не буде містити жодних методів,
     // а тільки властивості та конструктори для заповнення властивостей даними.
     // Цей клас буде структурою об’єкта – секція каталогу.
-    // Кожна секція каталогу повинна містити ім’я основної секції та ім’я підсекції. 
+    // Кожна секція каталогу повинна містити ім’я основної секції та номер.
     public class CatalogSection
     {
-
+        // Назва каталогу
+        public string CatalogTitle { get; set; }
+        // id каталогу
+        public int Catalog_ID { get; set; }
+        
+        // конструктор для заповнення інформації щодо назви каталогу та його номеру(ID)
+        public CatalogSection(string catalogTitle, int catalog_ID)
+        {
+            CatalogTitle = catalogTitle;
+            Catalog_ID = catalog_ID;
+        }
     }
 }
