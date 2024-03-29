0b9c633310b29b67121e12f735fcbe4f6be754c7
n1kys <lebednikita12@gmail.com>
2024-03-29 20:39:13 +0200

    added properties and a constructor for the Reader class

--- LibraryApp/ClasessForWorkWithData/Reader.cs
+++ LibraryApp/ClasessForWorkWithData/Reader.cs
@@ -9,6 +9,29 @@ namespace ClasessForWorkWithData
     // Клас - читач, який буде зберігати інформацію про читача, таку як ім'я, адреса, контактна інформація та список взятих книг
     public class Reader
     {
+        // Ім'я читача
+        public string Name { get; set; }
+        // Фамілія читача
+        public string Lastname { get; set; }
+        // Адреса читача
+        public string Address { get; set; }
+        // Номер телефону читача
+        public int PhoneNumber { get; set; }
+        // Електронна адреса читача
+        public string Email { get; set; }
+        // Інформація про активність читача в бібліотеці (дата початку та закінчення оренди та назва книги)
+        public List<int> ReadersBooks { get; set; }
 
+        // конструктор для заповнення інформації про читача, а саме: ім'я, фамілія, адреса, номер телефону, електронна адреса, та історія оренд книг читача
+        public Reader(string name, string lastname, string address, int phoneNumber, string email, List<int> readerBooks)
+        {
+            Name = name;
+            Lastname = lastname;
+            Address = address;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            ReadersBooks = readerBooks;
+        }
+        
     }
 }
